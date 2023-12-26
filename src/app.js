66fa46c8adc95a50f34ca6c9b9c98e1036f99c67
Srujan <accgotfame015@gmail.com>
2023-12-27 01:42:55 +0530

    setup project files-part 1

---
+++ src/app.js
@@ -0,0 +1,6 @@
+//in js,import can be done using 2 menthods->
+//commom js and module js
+//we prefer module js
+//--watch option in node for servers
+//nodemon-for server restart purpose
+//we use dev-dep->only in dev not in prod
